d0b5ae721164848dbd104a785b446d9158f78f24
Dillon Smith <>
2022-11-26 17:37:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#print("Hello World!")
+#print("Hello World\nHello World\nHello World")
+#print("Hello" + " Angela")
+#print("Hello" +" " + "Angela")
+#  input() will get user input in console
+#  Then print ()will print the word "Hello" and the user in
+#print("Hello " + input("What is your name?"))
+
+
+
